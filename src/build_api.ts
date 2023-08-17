5f6462a22cbb3b7b65f078f36f3a4085833d7872
Gantulga Khuyag <gantulgak@ot.mn>
2023-08-17 18:43:04 +0800

    Add TDS

--- src/build_api.ts
+++ src/build_api.ts
@@ -3,6 +3,28 @@
 import { log } from 'utils'
 import { existsSync } from 'node:fs'
 import { buildSync } from 'esbuild'
+import * as ts from "typescript"
+
+const compile = (fileNames: string[], options: ts.CompilerOptions): void => {
+    // Create a Program with an in-memory emit
+    const createdFiles = {}
+    const host = ts.createCompilerHost(options);
+    host.writeFile = (fileName: string, contents: string) => createdFiles[fileName] = contents
+
+    // Prepare and emit the d.ts files
+    const program = ts.createProgram(fileNames, options, host);
+    program.emit();
+
+    // Loop through all the input files
+    fileNames.forEach(file => {
+        console.log("### JavaScript\n")
+        console.log(host.readFile(file))
+
+        console.log("### Type Definition\n")
+        const dts = file.replace(".js", ".d.ts")
+        console.log(createdFiles[dts])
+    })
+}
 
 export const build_api = (cf: any, dir: string) => {
 
@@ -22,9 +44,16 @@ export const build_api = (cf: any, dir: string) => {
             outfile: output,
             bundle: true,
             minify: true,
-            sourcemap: true,
+            sourcemap: false,
             format: 'cjs',
         })
+
+        // Run the compiler
+        compile(process.argv.slice(2), {
+            allowJs: true,
+            declaration: true,
+            emitDeclarationOnly: true,
+        });
 
         cf.debug && log.info(`[ubin]: Building completed`)
 
