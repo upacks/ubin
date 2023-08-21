98cf5bb7a943c4b1dd8643ddf55394515434b581
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2023-08-21 11:33:44 +0000

    deploy: 85adb6badd111259109c4e5afab21eec6021052e

---
+++ bin/serve_app.d.ts
@@ -0,0 +1,2 @@
+#!/usr/bin/env node
+export declare const serve_app: (cf: any) => void;
---
+++ bin/serve_app.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.serve_app = void 0;
+const serve_app = (cf) => {
+    const { dir, outDir, debug, log, port } = cf;
+    try {
+        const entry = `${outDir}/index.js`;
+        debug && log.info(`Source ${entry}`);
+        const express = require("express");
+        const app = express();
+        app.use(express.static(`${dir}/dist`));
+        app.use(express.static(`${dir}/public`));
+        app.use((req, res, next) => res.sendFile(`${dir}/public/index.html`));
+        const l = app.listen(port, () => debug && log.info(`Started on port ${l.address().port}`));
+    }
+    catch (err) {
+        log.warn(err.message);
+    }
+};
+exports.serve_app = serve_app;
